337ccc5c5a0254e62d25b05250634fd9ee881464
FaishalAnis <mdfaisalanis@gmail.com>
2026-03-12 11:44:23 +0530

    First Commit for Sub

---
+++ src/com/subtract/Demo.java
@@ -0,0 +1,14 @@
+package com.subtract;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a= 10;
+		int b=50;
+		System.out.println(a+b);
+
+	}
+
+}
+
